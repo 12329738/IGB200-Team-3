992d8ad1ef5927b350d23fc9e38dcf1424a0af06
DevNight <n11022710@qut.edu.au>
2026-09-18 14:41:25 +1000

    Made incredibly basic and rough moving sky

---
+++ Assets/Code/Scripts/MovingSky.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class MovingSky : MonoBehaviour
+{
+    private float XPos;
+    private float XScale;
+
+    [SerializeField] private float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        XPos = transform.position.x;
+        XScale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveLeft();
+        XPos = transform.position.x;
+        CheckPosition(XPos);
+    }
+
+    private void MoveLeft()
+    {
+        transform.position += speed * Time.deltaTime * Vector3.left;
+        
+    }
+
+    private void CheckPosition(float XPosition)
+    {
+        Debug.Log(Screen.width);
+        if (XPosition <= -22f)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        transform.position = new Vector3(43.11f, 0, 0);
+        transform.localScale = new Vector3(transform.localScale.x * -1, 0.85f, 1);
+    }
+}
