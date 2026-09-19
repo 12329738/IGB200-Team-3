8df3507114c11c027d8dfe34b3a0143b93e06a9c
DevNight <n11022710@qut.edu.au>
2026-09-19 13:30:47 +1000

    changed direction of the moving sky
    So it makes sense with the implied wind from the animated grass

--- Assets/Code/Scripts/MovingSky.cs
+++ Assets/Code/Scripts/MovingSky.cs
@@ -17,21 +17,21 @@ public class MovingSky : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveLeft();
+        Move();
         XPos = transform.position.x;
         CheckPosition(XPos);
     }
 
-    private void MoveLeft()
+    private void Move()
     {
-        transform.position += speed * Time.deltaTime * Vector3.left;
+        transform.position += speed * Time.deltaTime * Vector3.right;
         
     }
 
     private void CheckPosition(float XPosition)
     {
         Debug.Log(Screen.width);
-        if (XPosition <= -22f)
+        if (XPosition >= 22f)
         {
             Flip();
         }
@@ -39,7 +39,7 @@ public class MovingSky : MonoBehaviour
 
     private void Flip()
     {
-        transform.position = new Vector3(43.11f, 0, 0);
+        transform.position = new Vector3(-43.11f, 0, 0);
         transform.localScale = new Vector3(transform.localScale.x * -1, 0.85f, 1);
     }
 }
